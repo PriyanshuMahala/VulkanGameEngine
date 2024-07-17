c7c683554b24465c290811465492320decb60d71
PriyanshuMahala <mahalapriyanshu34@gmail.com>
2024-07-17 09:27:09 +0530

    glew

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 
 
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    glewInit();
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -20,9 +20,13 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    /* Make the window's context current */ 
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glewInit failed" << '\n';
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
